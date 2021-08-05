66258d43b02d8f472f385db5b0288045af6bb638
Lance Avitia <la@Lances-Mac-mini.local>
2021-08-05 16:08:05 -0700

    fixing errors

--- pages/success.js
+++ pages/success.js
@@ -1,47 +1,4 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import useSWR from 'swr';
-import { useShoppingCart } from '@/hooks/use-shopping-cart';
-import { fetcher } from '@/lib/utils';
-
 const Success = () => {
-  const {
-    query: { session_id },
-  } = useRouter();
-
-  const { clearCart } = useShoppingCart();
-
-  const { data, error } = useSWR(
-    () => `/api/checkout_sessions/${session_id}`,
-    fetcher
-  );
-
-  useEffect(() => {
-    if (data) {
-      clearCart();
-    }
-  }, [data]);
-
-  return (
-    <div className="">
-      {error ? (
-        <div className="">
-          <p className="">Sorry, something went wrong!</p>
-        </div>
-      ) : !data ? (
-        <div className="">
-          <p className="">Loading...</p>
-        </div>
-      ) : (
-        <div className="">
-          <h2 className="">
-            <span>Thanks for your order!</span>
-          </h2>
-          <p className="">Check your inbox for the receipt.</p>
-        </div>
-      )}
-    </div>
-  );
-};
-
+  <div></div>
+}
 export default Success;
