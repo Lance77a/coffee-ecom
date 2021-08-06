9f6d306c0def87e3aeac04f3a19e8999185ea5e1
Lance Avitia <la@Lances-Mac-mini.local>
2021-08-05 22:59:44 -0700

    error

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
     <Layout>
       <CartProvider mode="payment"
       cartMode='client-only'
-      stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}
+      stripe={"pk_test_51JKYVWByqALDynDix3ePZb5cGSGvTqqmMQmeHIZmnIPV3YZt4hRftUAgZuV7R4HFcr4pPhdmHXf8bPsBXhn992HU00NqtKjhkt"}
       currency={"USD"}
       successUrl="https://stripe.com"
       cancelUrl="https://twitter.com/dayhaysoos"
